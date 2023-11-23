01d486c75c93f24e276aee7bf30ce99f6ae40092
Oliveira <rooan@live.com>
2023-11-22 21:20:48 -0300

    Update VisibilityControl.jsx

--- TPI_React_UTN/src/components/VisibilityControl.jsx
+++ TPI_React_UTN/src/components/VisibilityControl.jsx
@@ -11,19 +11,18 @@ export const VisibilityControl = ({
     }
   };
   return (
-    <div className="d-flex justify-content-between my-2 align-items-center">
-      <div>
+    <div className="d-flex justify-content-between my-2 align-items-center bg-secondary p-2">
+      <div className="form-check form-switch">
         <input
+          className="form-check-input"
           type="checkbox"
+          role="switch"
           checked={isChecked}
           onChange={(e) => setShowCompleted(e.target.checked)}
         />{" "}
         <label>Mostrar Completadas</label>
       </div>
-      <button
-        onClick={(e) => handleDelete()}
-        className="btn btn-primary btn-sm"
-      >
+      <button onClick={(e) => handleDelete()} className="btn btn-danger btn-sm">
         <i class="bi bi-trash3-fill"></i>
       </button>
     </div>
