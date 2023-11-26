9de0039154938b44d24919fdc3c7a05f891c6c50
Rooan <rooan@live.com>
2023-11-26 20:38:11 -0300

    Update edit.jsx

--- Noton_Route/src/routes/edit.jsx
+++ Noton_Route/src/routes/edit.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { Form, useLoaderData, redirect, useNavigate } from "react-router-dom";
 import { updateTask } from "../tasks";
 
@@ -13,34 +14,53 @@ export default function EditTask() {
   const navigate = useNavigate();
 
   return (
-    <Form method="post" id="task-form">
-      <p>
-        <span>Task Name</span>
+    <Form className="max-w-lg mx-auto mt-8" method="post" id="task-form">
+      <div className="mb-4">
+        <label className="block text-gray-700 text-sm font-bold mb-2">
+          Task Name
+        </label>
         <input
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
           placeholder="Name"
           aria-label="Task name"
           type="text"
           name="name"
           defaultValue={task.name}
         />
-      </p>
-      <label>
-        <span>Task Image URL</span>
+      </div>
+      <div className="mb-4">
+        <label className="block text-gray-700 text-sm font-bold mb-2">
+          Task Image URL
+        </label>
         <input
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
           placeholder="https://example.com/image.jpg"
           aria-label="Image URL"
           type="text"
           name="image"
           defaultValue={task.image}
         />
-      </label>
-      <label>
-        <span>Task Notes</span>
-        <textarea name="notes" defaultValue={task.notes} rows={6} />
-      </label>
-      <p>
-        <button type="submit">Save</button>
+      </div>
+      <div className="mb-4">
+        <label className="block text-gray-700 text-sm font-bold mb-2">
+          Task Notes
+        </label>
+        <textarea
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          name="notes"
+          defaultValue={task.notes}
+          rows={6}
+        />
+      </div>
+      <div className="mb-6">
         <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          type="submit"
+        >
+          Save
+        </button>
+        <button
+          className="ml-4 bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
           type="button"
           onClick={() => {
             navigate(-1);
@@ -48,7 +68,7 @@ export default function EditTask() {
         >
           Cancel
         </button>
-      </p>
+      </div>
     </Form>
   );
 }
